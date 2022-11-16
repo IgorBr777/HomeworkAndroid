a7d48d198ed43ebc24e37dfe895ac8f2ac8f04ba
IgorBr777 <igorbraz14@gmail.com>
2022-11-16 21:41:56 +0300

    HomeWork17, created ActionBar, Menu

--- app/src/main/java/com/example/homeworkandroid/MainActivity.kt
+++ app/src/main/java/com/example/homeworkandroid/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.homeworkandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.homeworkandroid.MainActivity2.Companion.startMainActivity2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+  }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.item_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.first_item -> startMainActivity2(this, "" )
+            R.id.second_item -> startMainActivity2(this, "" )
+            R.id.third_item -> onBackPressed()
+            R.id.four_item -> finishAffinity()
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/homeworkandroid/MainActivity2.kt
@@ -0,0 +1,44 @@
+package com.example.homeworkandroid
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        val  data:String = intent.getStringExtra(KEY)?:getString(R.string.no_data)
+        textView2.text=data
+
+        val textView3 = findViewById<TextView>(R.id.textView3)
+
+        val  data2:String = intent.getStringExtra(KEY2)?:getString(R.string.no_data)
+        textView3.text=data2
+        val  data3 = intent.getStringExtra(KEY3)
+        supportActionBar?.title = data3
+
+    }
+
+    companion object {
+        private const val KEY = "1"
+        private const val KEY2 = "2"
+        private const val KEY3 = "3"
+        fun startMainActivity2(context: Context, string: String) {
+            val intent = Intent(context, MainActivity2::class.java)
+            intent.putExtra(KEY, context.getString(R.string.welcome_main_activity_2))
+            intent.putExtra(KEY2, context.getString(R.string.hello_teach_me_skills))
+            intent.putExtra(KEY3, context.getString(R.string.clicked_second_item))
+            context.startActivity(intent)
+
+        }
+
+    }
+
+}
